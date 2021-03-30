3cf1cd23d0dec0826366ad59689f4290091c8f0c
ErikHedaker <erik@hedaker.se>
2021-03-30 10:06:23 +0200

    Added base functions to interact with database

---
+++ src/model/Profile.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Profile
+{
+    String name;
+    ArrayList<Plant> plants;
+
+    public Profile( String name )
+    {
+        this.name = name;
+    }
+
+    public Profile( String name, ArrayList<Plant> plants )
+    {
+        this.name = name;
+        this.plants = plants;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    public void setPlants( ArrayList<Plant> plants )
+    {
+        this.plants = plants;
+    }
+
+    @Override
+    public String toString( )
+    {
+        String output = name + "\n";
+        for( Plant plant : plants )
+        {
+            output += plant + "\n";
+        }
+        return output;
+    }
+}
