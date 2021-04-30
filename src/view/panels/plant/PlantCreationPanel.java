1163d0a793929edff7adc5263f25ed2191adde3d
Your Name <81147899+Rosenglim@users.noreply.github.com>
2021-04-30 23:01:08 +0200

    Update PlantCreationPanel.java
    this sux, please fix

--- src/view/panels/plant/PlantCreationPanel.java
+++ src/view/panels/plant/PlantCreationPanel.java
@@ -3,11 +3,18 @@ package view.panels.plant;
 import controller.Controller;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+
+/**
+    * The class sux, but yeah...
+    * @author Fandoggie
+    */
 
 public class PlantCreationPanel extends JPanel {
     private Controller controller;
-    private JLabel editLabel;
 
     public PlantCreationPanel(Controller controller){
         this.controller = controller;
@@ -18,13 +25,86 @@ public class PlantCreationPanel extends JPanel {
         setBackground(new Color(245, 245, 245));
         setBorder(BorderFactory.createEmptyBorder(50, 340, 50, 340));
 
-        editLabel = new JLabel("Edit");
+
+        setBorder(BorderFactory.createEmptyBorder(10, 340, 0, 340));
+        JPanel editPanel = new JPanel(new BorderLayout());
+
+        Border border = BorderFactory.createLineBorder(Color.lightGray, 1, true);
+        editPanel.setBorder(border);
+
+        Border margin1 = new EmptyBorder(50,50,50,50);
+        editPanel.setBorder(new CompoundBorder(editPanel.getBorder(), margin1));
+        editPanel.setBackground(Color.white);
+
+        JPanel sitePanel = new JPanel(new BorderLayout());
+        sitePanel.setBackground(Color.white);
+        JLabel editLabel = new JLabel("EDIT YOUR PLANT");
+        editLabel.setBorder(BorderFactory.createEmptyBorder(0,0,30,0));
+        editLabel.setBounds(10,10,10,10);
+        editLabel.setFont(new Font("Calibri light", Font.BOLD, 25));
+        sitePanel.add(editLabel, BorderLayout.NORTH);
 
 
+        JLabel plantLabel = new JLabel("Plant: ");
+        plantLabel.setFont(new Font("Calibri light", Font.PLAIN, 18));
+        sitePanel.add(plantLabel, BorderLayout.CENTER);
+
+        JTextField plantTF = new JTextField();
+        plantTF.setPreferredSize(new Dimension(200, 18));
+        sitePanel.add(plantTF, BorderLayout.EAST);
+        editPanel.add(sitePanel, BorderLayout.NORTH);
 
 
+        JPanel nicknamePanel = new JPanel(new BorderLayout());
+        nicknamePanel.setBackground(Color.white);
+
+        JLabel nicknameLabel = new JLabel("Nickname (optional): ");
+        nicknameLabel.setFont(new Font("Calibri light", Font.PLAIN, 18));
+        nicknamePanel.add(nicknameLabel, BorderLayout.WEST);
+
+        JTextField nicknameTF = new JTextField();
+        nicknameTF.setPreferredSize(new Dimension(200, 18));
+        nicknamePanel.add(nicknameTF, BorderLayout.EAST);
+        editPanel.add(nicknamePanel, BorderLayout.CENTER);
+
+        JPanel sizePanel = new JPanel(new BorderLayout());
+        sizePanel.setBackground(Color.white);
 
 
+        /*JLabel sizeLabel = new JLabel("Size: ");
+        sizeLabel.setFont(new Font("Calibri light", Font.PLAIN, 18));
+        sizePanel.add(sizeLabel, BorderLayout.WEST);
+
+        JCheckBox smallBox = new JCheckBox("Small");
+        JCheckBox mediumBox = new JCheckBox("Medium");
+        JCheckBox bigBox = new JCheckBox("Large");
+        sizePanel.add(smallBox);
+        sizePanel.add(mediumBox);
+        sizePanel.add(bigBox);
+        editPanel.add(sizePanel;
+
+        JPanel climatePanel = new JPanel(new BorderLayout());
+        climatePanel.setBackground(Color.white);
+
+        JLabel climateLabel = new JLabel("Climate: ");
+        climateLabel.setFont(new Font("Calibri light", Font.PLAIN, 18));
+        climatePanel.add(climateLabel, BorderLayout.WEST);
+
+        String[] choices = {"Wet room", "Outside", "Inside"};
+        JComboBox<String> climateMenu = new JComboBox<String>(choices);
+        climateMenu.setPreferredSize(new Dimension(200, 18));
+        climatePanel.add(climateMenu, BorderLayout.EAST);
+        editPanel.add(climatePanel;
+
+        JPanel savePanel = new JPanel(new BorderLayout());
+        savePanel.setBackground(Color.white);
+
+        JButton saveButton = new JButton("Save");
+        savePanel.add(saveButton, BorderLayout.SOUTH);
+        editPanel.add(savePanel, BorderLayout.SOUTH);*/
+
+
+        add(editPanel);
 
     }
 }
