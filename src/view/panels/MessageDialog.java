a235beace6b2d7d39bd2f37d085b747bdac47db3
vijoh01 <vijoh01@gmail.com>
2021-05-10 02:07:28 +0200

    Påbörjat MessageDialog

--- src/view/panels/MessageDialog.java
+++ src/view/panels/MessageDialog.java
@@ -1,24 +1,22 @@
 package view.panels;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.LineBorder;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.Locale;
 
 public class MessageDialog {
 
     public MessageDialog() {
-        JOptionPane.showConfirmDialog(null,
-                new JLabel("tihi"),
-                "JOptionPane Example : ",
-                JOptionPane.OK_CANCEL_OPTION,
-                JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null,
+                getPanel(),
+                "JOptionPane Example : ", JOptionPane.PLAIN_MESSAGE);
+    }
 
+    public JPanel getPanel() {
+        JPanel panel = new JPanel(new BorderLayout());
+        JButton btn = new JButton("TEST");
+        panel.add(btn);
+        return panel;
     }
 
 }
