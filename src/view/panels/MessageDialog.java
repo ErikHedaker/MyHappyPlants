5f0e9d4933427c915378e080bd56c74928986780
Your Name <81147899+Rosenglim@users.noreply.github.com>
2021-05-10 15:03:38 +0200

    Fibblade runt i MessageDialog.

--- src/view/panels/MessageDialog.java
+++ src/view/panels/MessageDialog.java
@@ -2,22 +2,29 @@ package view.panels;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.Locale;
+import java.awt.event.ActionEvent;
+import controller.Controller;
 
+import java.awt.event.ActionListener;
+import java.util.Locale;
 import static javax.swing.SwingConstants.CENTER;
 
-public class MessageDialog {
+
+public class MessageDialog implements ActionListener {
     private JLabel image;
+    private JButton yesBtn, noBtn;
+    private Controller controller;
+    private JDialog dialog;
 
     public MessageDialog() {
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         int screenWidth = (int) dim.getWidth();
         int screenHeight = (int) dim.getHeight();
 
-        JDialog dialog = new JDialog();
+        dialog = new JDialog();
         dialog.setSize(600, 400);
         dialog.setBackground(Color.white);
-        dialog.setLocation(screenWidth / 2 - 150, screenHeight / 2 - 150);
+        dialog.setLocation(screenWidth / 2 - 300, screenHeight / 2 - 150);
         dialog.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(0,3,30,3);
@@ -48,11 +55,12 @@ public class MessageDialog {
         buttons.setBackground(Color.white);
         buttons.setBorder(BorderFactory.createEmptyBorder(20, 0, 10, 0));
 
-        JButton yesBtn = new JButton("YES");
+        yesBtn = new JButton("YES");
         yesBtn.setVisible(true);
         yesBtn.setFont(new Font("Arial", Font.BOLD, 18));
         yesBtn.setBackground(new Color(220, 229, 185));
         yesBtn.setForeground(Color.darkGray);
+        yesBtn.addActionListener(this);
         yesBtn.setFocusPainted(false);
         yesBtn.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(93, 118, 77)));
         yesBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -62,11 +70,12 @@ public class MessageDialog {
         gbc.anchor = GridBagConstraints.WEST;
         dialog.add(yesBtn, gbc);
 
-        JButton noBtn = new JButton("NO");
+        noBtn = new JButton("NO");
         noBtn.setVisible(true);
         noBtn.setFont(new Font("Arial", Font.BOLD, 18));
         noBtn.setBackground(new Color(220, 229, 185));
         noBtn.setForeground(Color.darkGray);
+        noBtn.addActionListener(this);
         noBtn.setFocusPainted(false);
         noBtn.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, new Color(93, 118, 77)));
         noBtn.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -77,13 +86,22 @@ public class MessageDialog {
         dialog.add(noBtn, gbc);
 
 
+        dialog.setVisible(false);
 
-        dialog.setVisible(true);
 
         /*JOptionPane.showMessageDialog(null,
                 getPanel(),
                 "JOptionPane Example : ", JOptionPane.PLAIN_MESSAGE);*/
     }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == yesBtn) {
+            controller.buttonPushed("remove plant");
+        } else if (e.getSource() == noBtn) {
+            controller.buttonPushed("plantList");
+        }
+
+
 
     /*public void setImageIcon(ImageIcon imageIcon) {
         Image img = imageIcon.getImage().getScaledInstance(150, 145,
@@ -98,5 +116,5 @@ public class MessageDialog {
         panel.add(btn);
         return panel;
     }*/
-
+    }
 }
